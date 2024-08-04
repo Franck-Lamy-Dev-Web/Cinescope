349312d29bc7fcc12c0fe20b4a5cca075c71ef1e
Franck Lamy <francklamymagie@gmail.com>
2024-08-04 19:22:53 +0200

    correction bug searchFilm

--- src/Componants/SearchFilm.jsx
+++ src/Componants/SearchFilm.jsx
@@ -15,14 +15,14 @@ function SearchFilm() {
       }
 
       try {
-        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}`);
+        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?language=fr-FR&api_key=${API_KEY}&query=${searchTerm}`);
         const result = response.data.results;
 
         const filmsData = result
           .filter(film => film.poster_path)
           .map(film => ({
             id: film.id,
-            titre: film.title,
+            title: film.title,
             overview: film.overview,
             release_date: film.release_date,
             poster_path: `https://image.tmdb.org/t/p/w500/${film.poster_path}`
@@ -45,7 +45,7 @@ function SearchFilm() {
           <input 
             className='form-control w-50' 
             type="text" 
-            placeholder="Titre de film" 
+            placeholder="Titre du film" 
             value={searchTerm}
             onChange={(event) => setSearchTerm(event.target.value)} 
           />
